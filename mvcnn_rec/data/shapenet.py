aca5498124e95e9d7b254b15ddd7d55114e4b269
Barış Zöng <bariszongur1@gmail.com>
2021-12-29 16:02:21 +0100

    small modifications

--- mvcnn_rec/data/shapenet.py
+++ mvcnn_rec/data/shapenet.py
@@ -5,8 +5,9 @@ import cv2
 import numpy as np
 import torch
 from config import cfg
-
+from torchvision import transforms
 from mvcnn_rec.data.binvox_rw import read_as_3d_array
+from PIL import Image
 
 class ShapeNetMultiview(torch.utils.data.Dataset):
     """Class for loading multiview images, class, and voxel of object
@@ -16,7 +17,21 @@ class ShapeNetMultiview(torch.utils.data.Dataset):
         assert split in ['train', 'test', 'val']
         self.items = []
         self.classes = {}
-
+        self.class_ids = {
+            "aeroplane" : 0,
+            "bench" : 1,
+            "cabinet": 2,
+            "car": 3,
+            "chair": 4,
+            "display": 5,
+            "lamp": 6,
+            "speaker": 7,
+            "rifle": 8,
+            "sofa": 9,
+            "table": 10,
+            "telephone": 11,
+            "watercraft": 12
+        }
         with open(cfg.DATASETS.SHAPENET.TAXONOMY_FILE_PATH, encoding='utf-8') as file:
             content = file.read()
 
@@ -40,7 +55,7 @@ class ShapeNetMultiview(torch.utils.data.Dataset):
             "name": item,
             "item": images,
             "voxel": voxel[np.newaxis, :, :, :],
-            "label": self.classes[item_class]
+            "label": self.class_ids[self.classes[item_class]]
         }
 
     def __len__(self):
@@ -55,35 +70,31 @@ class ShapeNetMultiview(torch.utils.data.Dataset):
 
     @staticmethod
     def get_images(item):
-        #taxonomy_folder_name = item.split('/')[0]
-        #sample_name = item.split('/')[1]
-        ## Get file list of rendering images
-        #img_file_path = cfg.DATASETS.SHAPENET.RENDERING_PATH % (taxonomy_folder_name, sample_name, 0)
-        #img_folder = os.path.dirname(img_file_path)
-        #total_views = len(os.listdir(img_folder))
-        #rendering_image_indexes = range(total_views)
-        #rendering_images_file_path = []
-        #for image_idx in rendering_image_indexes:
-        #    img_file_path = cfg.DATASETS.SHAPENET.RENDERING_PATH % (taxonomy_folder_name, sample_name, image_idx)
-        #    if not os.path.exists(img_file_path):
-        #        continue
-        #    rendering_images_file_path.append(img_file_path)
-
-        #file_list = []
-        rendering_images = []
+        preprocessed_images = []
+        preprocess = transforms.Compose([
+            transforms.Resize(222),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        ])
         for i in range(0, 23):
             img_path = cfg.DATASETS.SHAPENET.RENDERING_PATH % (item.split('/')[0], item.split('/')[1], i)
             #print(f"open img_path: {img_path}")
-            image = cv2.imread(img_path, cv2.IMREAD_UNCHANGED)
+            #image = cv2.imread(img_path, cv2.IMREAD_UNCHANGED)
             #cv2.imshow('image', image)
             #cv2.waitKey(0)
-            rendering_image = cv2.imread(img_path, cv2.IMREAD_UNCHANGED).astype(np.float32) / 255.
+            #rendering_image = cv2.imread(img_path, cv2.IMREAD_UNCHANGED).astype(np.float32) / 255.
+            #rendering_image = preprocess(cv2.resize(cv2.imread(img_path, cv2.IMREAD_UNCHANGED), (222, 222),
+            #                            interpolation=cv2.INTER_NEAREST).astype(np.float32) )
+            #rendering_image = preprocess(rendering_image)
             #print(rendering_image.shape)
-            rendering_images.append(rendering_image)
-
-        return np.asarray(rendering_images)
+            input_image = Image.open(img_path)
+            input_tensor = preprocess(input_image)
+            preprocessed_images.append(input_tensor)
+        return np.asarray(preprocessed_images)
 
 
     @staticmethod
     def move_batch_to_device(batch, device):
-        raise NotImplementedError
+        batch["item"] = batch['item'].to(device, torch.float32)
+        batch['voxel'] = batch['voxel'].to(device, torch.float32)
+        batch['label'] = batch['label'].to(device, torch.float32)
