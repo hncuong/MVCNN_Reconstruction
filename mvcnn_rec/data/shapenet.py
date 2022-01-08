28cf53e8695f9411bfb3efe4d849d89d28e7686c
Nhat Cuong Ha <nhatcuong94@gmail.com>
2022-01-08 15:44:55 +0100

    Update dataset to load desired num views. Fix small bugs on images loading (rgb) and move batch. Add overfit dataset.

--- mvcnn_rec/data/shapenet.py
+++ mvcnn_rec/data/shapenet.py
@@ -10,53 +10,79 @@ from mvcnn_rec.data.binvox_rw import read_as_3d_array
 from PIL import Image
 
 class ShapeNetMultiview(torch.utils.data.Dataset):
-    """Class for loading multiview images, class, and voxel of object
+    """ 
+    Class for loading multiview images, class, and voxel of object
     """
-    def __init__(self, split):
+    num_classes = 13
+    class_ids = {
+        "aeroplane" : 0,
+        "bench" : 1,
+        "cabinet": 2,
+        "car": 3,
+        "chair": 4,
+        "display": 5,
+        "lamp": 6,
+        "speaker": 7,
+        "rifle": 8,
+        "sofa": 9,
+        "table": 10,
+        "telephone": 11,
+        "watercraft": 12
+    }
+    id_class_mapping = {idx: class_name for class_name, idx in class_ids.items()}
+
+    def __init__(self, split, total_views=24, num_views=24, load_mode='mvcnn_rec', random_start_view=False):
+        """
+        Arguments: 
+        - total_views: total views per shape; for shapenet: 24
+        - num_views: number of view images per shape. Must be 24, 12, 8, 6, 4, 3, 2 or 1
+        - load_mode: mvcnn_rec - load images, labels and voxels; mvcnn - load only images and labels
+        """
         super().__init__()
-        assert split in ['train', 'test', 'val']
+        assert split in ['train', 'test', 'val', 'overfit']
         self.items = []
         self.classes = {}
-        self.class_ids = {
-            "aeroplane" : 0,
-            "bench" : 1,
-            "cabinet": 2,
-            "car": 3,
-            "chair": 4,
-            "display": 5,
-            "lamp": 6,
-            "speaker": 7,
-            "rifle": 8,
-            "sofa": 9,
-            "table": 10,
-            "telephone": 11,
-            "watercraft": 12
-        }
+        
+        self.total_views = total_views
+        self.num_views = num_views
+        self.stride = self.total_views // self.num_views
+        self.load_mode = load_mode
+        self.random_start_view = random_start_view
         with open(cfg.DATASETS.SHAPENET.TAXONOMY_FILE_PATH, encoding='utf-8') as file:
             content = file.read()
 
-        print(cfg.DATASETS.SHAPENET.TAXONOMY_FILE_PATH)
+        # print(cfg.DATASETS.SHAPENET.TAXONOMY_FILE_PATH)
         dataset_taxonomy = json.loads(content)
         #print(dataset_taxonomy)
 
         for taxonomy in dataset_taxonomy:
             self.classes[taxonomy['taxonomy_id']] = taxonomy['taxonomy_name']
             #print('"%s": "%s"' % (taxonomy['taxonomy_name'], taxonomy['taxonomy_id']))
-            for item in taxonomy[split]:
+            for item in taxonomy.get(split, []):
                 self.items.append("%s/%s" % (taxonomy['taxonomy_id'], item))
 
     def __getitem__(self, index):
         item = self.items[index]
-        print(f"item: {item}")
+        # print(f"item: {item}")
         item_class = item.split('/')[0]
-        images = ShapeNetMultiview.get_images(item)
-        voxel = ShapeNetMultiview.get_voxel(item)
-        return {
-            "name": item,
-            "item": images,
-            "voxel": voxel[np.newaxis, :, :, :],
-            "label": self.class_ids[self.classes[item_class]]
-        }
+        start_view_idx = 0
+        if self.random_start_view:
+            start_view_idx = np.random.randint(0, self.stride)
+        images = ShapeNetMultiview.get_images(item, self.num_views, self.total_views, start_view_idx)
+        if (self.load_mode == 'mvcnn_rec'):
+            voxel = ShapeNetMultiview.get_voxel(item)
+            return {
+                "name": item,
+                "item": images,
+                "voxel": voxel[np.newaxis, :, :, :], # TODO check if should be new_axis here.
+                "label": ShapeNetMultiview.class_ids[self.classes[item_class]]
+            }
+        else:
+            return {
+                "name": item,
+                "item": images,
+                "label": ShapeNetMultiview.class_ids[self.classes[item_class]]
+            }
 
     def __len__(self):
         return len(self.items)
@@ -69,32 +95,28 @@ class ShapeNetMultiview(torch.utils.data.Dataset):
         return voxels
 
     @staticmethod
-    def get_images(item):
+    def get_images(item, num_views=24, total_views=24, start_idx=0):
         preprocessed_images = []
+        # TODO: different transform on Train and Test mode
         preprocess = transforms.Compose([
-            transforms.Resize(222),
+            transforms.Resize(224), # 224 for Resnet
             transforms.ToTensor(),
-            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]), # Correct normalize. # output[i] = (input[i] - mean) / std
         ])
-        for i in range(0, 23):
+        # TODO: Custome num views 24, 12, 8, 6, 4, 3, 2, 1
+        stride = total_views // num_views
+        for i in range(start_idx, total_views, stride):
             img_path = cfg.DATASETS.SHAPENET.RENDERING_PATH % (item.split('/')[0], item.split('/')[1], i)
-            #print(f"open img_path: {img_path}")
-            #image = cv2.imread(img_path, cv2.IMREAD_UNCHANGED)
-            #cv2.imshow('image', image)
-            #cv2.waitKey(0)
-            #rendering_image = cv2.imread(img_path, cv2.IMREAD_UNCHANGED).astype(np.float32) / 255.
-            #rendering_image = preprocess(cv2.resize(cv2.imread(img_path, cv2.IMREAD_UNCHANGED), (222, 222),
-            #                            interpolation=cv2.INTER_NEAREST).astype(np.float32) )
-            #rendering_image = preprocess(rendering_image)
-            #print(rendering_image.shape)
-            input_image = Image.open(img_path)
+            input_image = Image.open(img_path).convert('RGB')
             input_tensor = preprocess(input_image)
             preprocessed_images.append(input_tensor)
-        return np.asarray(preprocessed_images)
+        return torch.stack(preprocessed_images) # return a tensor
 
 
     @staticmethod
-    def move_batch_to_device(batch, device):
+    def move_batch_to_device(batch, device, load_mode='mvcnn_rec'):
         batch["item"] = batch['item'].to(device, torch.float32)
-        batch['voxel'] = batch['voxel'].to(device, torch.float32)
-        batch['label'] = batch['label'].to(device, torch.float32)
+        batch['label'] = batch['label'].to(device, torch.long)
+        if load_mode == 'mvcnn_rec':
+            batch['voxel'] = batch['voxel'].to(device, torch.float32)
+
